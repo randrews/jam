50f09df54e4c44b226c5f04b53a6ac9782358367
Ross Andrews <randrews@kallisti.local>
2009-07-21 22:58:00 -0500

    Refactored the ignore command to use the list_file module

--- commands/ignore.rb
+++ commands/ignore.rb
@@ -1,4 +1,7 @@
+require(Jam::JAM_DIR+"/lib/list_file.rb")
+
 class Jam::IgnoreCommand < Jam::Command
+  include Jam::ListFile
   include Jam::IgnoresFile
 
   def parse_options opts
@@ -24,27 +27,9 @@ class Jam::IgnoreCommand < Jam::Command
         emit(i)
       end
     elsif command_opts[:delete]
-      user_ignores=[]
-
-      File.open(dotjam('user_ignores'),'r') do |file|
-        file.each_line{|l| user_ignores << l.strip }
-      end
-
-      new_ignores = user_ignores - targets
-
-      File.open(dotjam('user_ignores'),'w') do |file|
-        new_ignores.each do |i|
-          file << i
-          file << "\n"
-        end
-      end
+      remove_from_file(dotjam('user_ignores'),*targets)
     else
-      File.open(dotjam('user_ignores'),'a') do |file|
-        targets.each do |t|
-          file << t
-          file << "\n"
-        end
-      end
+      add_to_file(dotjam('user_ignores'),*targets)
     end
   end
 end
